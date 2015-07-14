3b91f3d4353b9df8d2e46a5d5371edaa400e9902
markin2000 <liran.markin@gmail.com>
2015-07-14 19:54:50 +0300

    adding examples

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,19 +6,54 @@ $('#input').bind("input change",function(){
     execute_program();
     console.log("changed");
 });
-//TODO: add coding examples
+var examples_dir = "examples";
 $('#example1').click(function(){
-
+    $.ajax({
+        url : examples_dir+"/example1.txt",
+        dataType: "text",
+        success : function (data) {
+            $("#code").val(data);
+        }
+    });
+    execute_program();
 });
 $('#example2').click(function(){
-
+    $.ajax({
+        url : examples_dir+"/example2.txt",
+        dataType: "text",
+        success : function (data) {
+            $("#code").val(data);
+        }
+    });
+    execute_program();
 });
 $('#example3').click(function(){
-
+    $.ajax({
+        url : examples_dir+"/example3.txt",
+        dataType: "text",
+        success : function (data) {
+            $("#code").val(data);
+        }
+    });
+    execute_program();
 });
 $('#example4').click(function(){
-
+    $.ajax({
+        url : examples_dir+"/example4.txt",
+        dataType: "text",
+        success : function (data) {
+            $("#code").val(data);
+        }
+    });
+    execute_program();
 });
 $('#example5').click(function(){
-
+    $.ajax({
+        url : examples_dir+"/example5.txt",
+        dataType: "text",
+        success : function (data) {
+            $("#code").val(data);
+        }
+    });
+    execute_program();
 });
